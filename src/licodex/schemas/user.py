9cc43ee94eec131ee9490f4be59bdce652b47c22
diogo <d.ogobaltazar+github@gmail.com>
2025-09-29 12:55:23 +0000

    feat: pydantic V2 attribute population

--- src/licodex/schemas/user.py
+++ src/licodex/schemas/user.py
@@ -2,21 +2,19 @@ import uuid
 from datetime import datetime
 from pydantic import BaseModel, EmailStr
 from typing import Optional
-from .organisation import OrganisationRead
 from typing import Literal
+from .organisation import OrganisationRead
+from .base import ORMBase
 
 class UserCreate(BaseModel):
     email: EmailStr
     role: Literal["user", "admin"] = "user"
 
-class UserRead(BaseModel):
+class UserRead(ORMBase):
     id: uuid.UUID
     email: EmailStr
     role: str
     created_at: datetime
-
-    class Config:
-        from_attributes = True
 
 
 class UserEmailUpdate(BaseModel):
